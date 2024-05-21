12a843435a6c0d576618fcb1021b6bf406e1d224
Marouani  Haythem <haythemmarouani@gmail.com>
2024-05-21 22:44:29 +0100

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -23,33 +23,25 @@ provider "azurerm" {
   # tenant_id       = var.tenant_id
 }
 
-locals {
-  default_resource_group_name = "rg-${var.resource_group_name}-t-001"
-  default_app_service_plan_name = "asp-${var.app_service_plan_name}-t-001"
-  default_app_service_name = "wa-${var.app_service_name}-t-001"
-}
 
-
-# Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-haythem-example-resources"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-module "app_service_plan" {
-  source                   = "./modules/app service plan"
-  app_service_plan_name    = "asp-haythemmarouani-t-002"
-  location                 = "West Europe"
-  resource_group_name      = "rg-haythem-example-resources"
-  app_service_plan_tier    = "Standard"
-  app_service_plan_size    = "F1"
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
 
-
-module "app_service" {
-  source                    = "./modules/app service"
-  app_service_name          = "wa-haythemmarouani-t"
-  location                 = "West Europe"
-  resource_group_name      = "rg-haythem-example-resources"
-  app_service_plan_id       = module.app_service_plan.app_service_plan_id
+resource "azurerm_app_service" "app_service" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
 }
