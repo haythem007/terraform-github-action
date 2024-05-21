d1e7cefdf4d6745cd0562763eac0a42443bb26ec
Marouani  Haythem <haythemmarouani@gmail.com>
2024-05-21 23:09:00 +0100

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -17,6 +17,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+ storage_account {
+    lock {
+      timeout = "10m"
+    }
+  }
   # subscription_id = var.subscription_id
   # client_id       = var.client_id
   # client_secret   = var.client_secret
