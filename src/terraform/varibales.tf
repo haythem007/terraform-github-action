adb4ade171bc35b68996ac1cb903b62b37931cc1
Marouani  Haythem <haythemmarouani@gmail.com>
2024-05-21 22:43:01 +0100

    Update varibales.tf

--- src/terraform/varibales.tf
+++ src/terraform/varibales.tf
@@ -1,48 +1,19 @@
 variable "resource_group_name" {
+  description = "The name of the resource group"
   type        = string
-  description = "The name of the resource group."
-  default = "rg-haythem-example-resources"
 }
 
-
 variable "location" {
+  description = "The location of the resource group"
   type        = string
-  description = "The Azure region where resources will be created."
-  default = "westeurope"
 }
 
 variable "app_service_plan_name" {
+  description = "The name of the App Service Plan"
   type        = string
-  description = "The name of the App Service Plan."
- 
 }
 
 variable "app_service_name" {
+  description = "The name of the App Service"
   type        = string
-  description = "The name of the App Service."
-
 }
-
-
-variable "app_service_plan_kind" {
-  description = "The kind of App Service Plan."
-  default     = "Windows"
-}
-
-variable "app_service_plan_tier" {
-  description = "The tier of the App Service Plan."
-  default     = "Standard"
-}
-
-variable "app_service_plan_size" {
-  description = "The size of the App Service Plan."
-  default     = "S1"
-}
-
-
-
-
-
-
-
-# Add other variables with types as needed for your project
