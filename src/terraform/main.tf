3f22aff345b0757140fe39ab66b5518076aed345
Marouani  Haythem <haythemmarouani@gmail.com>
2024-05-21 22:13:28 +0100

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
  backend "azurerm" {
-    resource_group_name  = "terraformrg"
-    storage_account_name = "terraformstorage7c6c24c7"
+    resource_group_name  = "rg-haythem-friday"
+    storage_account_name = "sttfstatemgt01"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
@@ -23,9 +23,33 @@ provider "azurerm" {
   # tenant_id       = var.tenant_id
 }
 
+locals {
+  default_resource_group_name = "rg-${var.resource_group_name}-t-001"
+  default_app_service_plan_name = "asp-${var.app_service_plan_name}-t-001"
+  default_app_service_name = "wa-${var.app_service_name}-t-001"
+}
+
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-haythem-example-resources"
   location = "West Europe"
+}
+
+module "app_service_plan" {
+  source                   = "./modules/app service plan"
+  app_service_plan_name    = local.default_app_service_plan_name
+  location                 = var.location
+  resource_group_name      = var.resource_group_name
+  app_service_plan_tier    = var.app_service_plan_tier
+  app_service_plan_size    = var.app_service_plan_size
+}
+
+
+module "app_service" {
+  source                    = "./modules/app service"
+  app_service_name          = local.default_app_service_name
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  app_service_plan_id       = module.app_service_plan.app_service_plan_id
 }
