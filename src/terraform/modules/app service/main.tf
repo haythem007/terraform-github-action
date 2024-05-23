4e4564fb3f5fa7443a98fa5c470b1e23244938d8
Marouani  Haythem <haythemmarouani@gmail.com>
2024-05-23 20:16:27 +0100

    Update main.tf

--- src/terraform/modules/app service/main.tf
+++ src/terraform/modules/app service/main.tf
@@ -2,9 +2,29 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_app_service" "example" {
+resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
+}
+
+variable "app_service_name" {
+  description = "Name of the App Service"
+  type        = string
+}
+
+variable "location" {
+  description = "Location"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+  type        = string
+}
+
+variable "app_service_plan_id" {
+  description = "ID of the App Service Plan"
+  type        = string
 }
