bed15ffcdf14471677730890031cb6d4308cdef6
Marouani  Haythem <haythemmarouani@gmail.com>
2024-05-23 20:19:13 +0100

    Update varibales.tf

--- src/terraform/varibales.tf
+++ src/terraform/varibales.tf
@@ -1,19 +1,31 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Name of the resource group"
   type        = string
 }
 
 variable "location" {
-  description = "The location of the resource group"
+  description = "Location of the resource group"
   type        = string
 }
 
 variable "app_service_plan_name" {
-  description = "The name of the App Service Plan"
+  description = "Name of the App Service Plan"
   type        = string
 }
 
 variable "app_service_name" {
-  description = "The name of the App Service"
+  description = "Name of the App Service"
   type        = string
+}
+
+variable "sku_tier" {
+  description = "SKU Tier for App Service Plan"
+  type        = string
+  default     = "Basic"
+}
+
+variable "sku_size" {
+  description = "SKU Size for App Service Plan"
+  type        = string
+  default     = "B1"
 }
